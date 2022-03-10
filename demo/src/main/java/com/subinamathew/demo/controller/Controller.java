5cdd041da91da86bb0c24efc40cba22b28be5aa7
subinamathew <smathew2237@gmail.com>
2022-03-10 14:43:00 +0000

    init: added hello world

---
+++ demo/src/main/java/com/subinamathew/demo/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.subinamathew.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String hello() {
+        return "hi!!";
+    }
+}
