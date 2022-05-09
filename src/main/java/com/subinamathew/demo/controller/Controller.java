36f3e9d01c2bbfccaa4d07b8a28c9f254c6c406b
subinamathew <smathew2237@gmail.com>
2022-05-09 20:23:38 +0000

    changed controller

--- src/main/java/com/subinamathew/demo/controller/Controller.java
+++ src/main/java/com/subinamathew/demo/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.subinamathew.demo.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +17,23 @@ public class Controller {
     }
 
     @GetMapping("/api")
-    public Map<String,Map> api() {
-        HashMap<String, Map> map = new HashMap<>();
+    public List<Map<String, String>> api() {
+
+        List<Map<String, String>> listOfMaps = new ArrayList<Map<String, String>>();
+
         HashMap<String, String> detail = new HashMap<>();
         detail.put("url", "https://github.com/subinamathew/springboot-helloworld");
         detail.put("kind", "Springboot");
-        map.put("backend",detail );
+        detail.put("instance", "backend");
+        listOfMaps.add(detail);
+
         HashMap<String, String> detail2 = new HashMap<>();
         detail2.put("url", "https://gitlab.com/gitpod-examples/fe-angular");
         detail2.put("kind", "Angular");
-        map.put("frontend",detail2 );
-        return map;
+        detail.put("instance", "frontend");
+
+        listOfMaps.add(detail2);
+        
+        return listOfMaps;
     }
 }
